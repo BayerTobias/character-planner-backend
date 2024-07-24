ec7ff4c4b29f5ad2829e0da8857371671c85a941
BayerTobias <tbayer2@gmx.de>
2024-07-24 14:49:00 +0200

    users post view (signup)

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, Users
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("users/", Users.as_view(), name="users"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken, APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from .models import CustomUser
 
 
 class LoginView(ObtainAuthToken):
@@ -14,3 +15,32 @@ class LoginView(ObtainAuthToken):
         token, created = Token.objects.get_or_create(user=user)
 
         return Response({"token": token.key}, status=status.HTTP_200_OK)
+
+
+class Users(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if CustomUser.objects.filter(username=username).exists():
+            return Response(
+                {"message": "This username already exists"},
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        if CustomUser.objects.filter(email=email).exists():
+            return Response(
+                {"message": "This email already exists"},
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        user = CustomUser.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        return Response(
+            {"message": "User created successfully"}, status=status.HTTP_201_CREATED
+        )
