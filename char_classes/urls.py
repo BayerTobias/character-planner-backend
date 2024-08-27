bbe07474afb43e437ec04cf5769e52eb76855cd0
BayerTobias <tbayer2@gmx.de>
2024-08-27 20:23:24 +0200

    CharClassDetailsView

--- char_classes/urls.py
+++ char_classes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import CharClassView
+from .views import CharClassView, CharClassDetailsView
 
 urlpatterns = [
     path("classes/", CharClassView.as_view(), name="char-class"),
+    path(
+        "classes/<int:id>/", CharClassDetailsView.as_view(), name="char-class-details"
+    ),
 ]
--- char_classes/views.py
+++ char_classes/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from .models import CharClass
-from .serializers import CharClassSerializer
+from .serializers import CharClassSerializer, CharClassDetailsSerializer
+from django.shortcuts import get_object_or_404
 
 
 class CharClassView(APIView):
@@ -12,3 +13,12 @@ class CharClassView(APIView):
         queryset = CharClass.objects.all()
         serializer = CharClassSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharClassDetailsView(APIView):
+
+    def get(self, request, id):
+
+        char_class = get_object_or_404(CharClass, pk=id)
+        serialiter = CharClassDetailsSerializer(char_class)
+        return Response(serialiter.data, status=status.HTTP_200_OK)
