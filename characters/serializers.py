e556ff7942b34d1e5e6eb9c04e64d9ccbed85600
BayerTobias <tbayer2@gmx.de>
2024-09-05 20:32:39 +0200

    create character first iteration

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from .models import Character, CharacterSkill
+from .models import Character, CharacterSkill, CharRace, CharClass
 from char_classes.serializers import CharClassDetailsSerializer
 from char_races.serializers import CharRaceSerializer
 from skills.serializers import CustomSkillSerializer
+from items.models import BaseWeapon, CustomWeapon, WeaponGroup, BaseArmor, Money
 from items.serializers import (
     BaseWeaponSerializer,
     CustomWeaponSerializer,
@@ -25,7 +26,7 @@ class CharacterSerializer(serializers.ModelSerializer):
     custom_skills = CustomSkillSerializer(many=True, read_only=True)
     race = CharRaceSerializer(read_only=True)
     base_weapons = BaseWeaponSerializer(many=True, read_only=True)
-    custom_weapons = serializers.SerializerMethodField()
+    custom_weapons = CustomWeaponSerializer(many=True, read_only=True)
     armor = BaseArmorSerializer(read_only=True)
     money = MoneySerializer(read_only=True)
 
@@ -33,10 +34,37 @@ class CharacterSerializer(serializers.ModelSerializer):
         model = Character
         fields = "__all__"
 
-    def get_custom_weapons(self, obj):
+    # def get_custom_weapons(self, obj):
 
-        custom_weapons = obj.custom_weapons.all()
-        return CustomWeaponSerializer(custom_weapons, many=True).data
+    #     custom_weapons = obj.custom_weapons.all()
+    #     return CustomWeaponSerializer(custom_weapons, many=True).data
+
+
+class CreateCharacterSerializer(serializers.ModelSerializer):
+
+    race = serializers.PrimaryKeyRelatedField(queryset=CharRace.objects.all())
+    char_class = serializers.PrimaryKeyRelatedField(queryset=CharClass.objects.all())
+    base_weapons = serializers.PrimaryKeyRelatedField(
+        queryset=BaseWeapon.objects.all(), many=True, allow_null=True, required=False
+    )
+    armor = serializers.PrimaryKeyRelatedField(
+        queryset=BaseArmor.objects.all(), allow_null=True, required=False
+    )
+    user = serializers.PrimaryKeyRelatedField(read_only=True)  # User als read-only
+    # money = MoneySerializer()
+
+    class Meta:
+        model = Character
+        fields = "__all__"
+
+    def create(self, validated_data):
+
+        base_weapons_data = validated_data.pop("base_weapons", [])
+        user = self.context["request"].user
+        character = Character.objects.create(user=user, **validated_data)
+        character.base_weapons.set(base_weapons_data)
+
+        return character
 
 
 class CharacterListSerializer(serializers.ModelSerializer):
--- characters/views.py
+++ characters/views.py
@@ -4,13 +4,28 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .models import Character
-from .serializers import CharacterSerializer, CharacterListSerializer
+from .serializers import (
+    CharacterSerializer,
+    CharacterListSerializer,
+    CreateCharacterSerializer,
+)
 from django.shortcuts import get_object_or_404
 
 
 class CharactersView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = CreateCharacterSerializer(
+            data=request.data, context={"request": request}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, character_id=None):
         if character_id:
