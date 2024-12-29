ac47a49faa9057140cf9ffb3483e94eb9d842f25
BayerTobias <tbayer2@gmx.de>
2024-12-29 19:28:01 +0100

    main_stat in class model

--- char_classes/models.py
+++ char_classes/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 
 class CharClass(models.Model):
+    STAT_CHOICES = [
+        ("strength", "Strength"),
+        ("agility", "Agility"),
+        ("constitution", "Constitution"),
+        ("intelligence", "Intelligence"),
+        ("charisma", "Charisma"),
+    ]
+
     name = models.CharField(max_length=25)
     base_lvl_hp = models.IntegerField(default=1)
     base_lvl_mana = models.IntegerField(null=True, blank=True)
+    main_stat = models.CharField(
+        max_length=20, choices=STAT_CHOICES, null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
